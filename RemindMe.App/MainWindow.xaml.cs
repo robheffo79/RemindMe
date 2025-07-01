3773f5777e0d6485ca984288b95bbf48bed70657
Robert Heffernan <robert@webmatrix.com.au>
2025-07-01 13:38:51 +1000

    Fix .b64 hack

--- RemindMe.App/MainWindow.xaml.cs
+++ RemindMe.App/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace RemindMe
 {
     public partial class MainWindow : Window
     {
-        private IHost? _serverHost;
+        private WebApplication _serverHost;
 
         public MainWindow()
         {
@@ -27,14 +27,11 @@ namespace RemindMe
         {
             _serverHost = CreateServer();
             await _serverHost.StartAsync();
-            var addresses = _serverHost.Services.GetRequiredService<IWebHost>().ServerFeatures
-                .Get<Microsoft.AspNetCore.Hosting.Server.Features.IServerAddressesFeature>();
-            var address = addresses.Addresses.First();
             await WebView.EnsureCoreWebView2Async();
-            WebView.CoreWebView2.Navigate(address);
+            WebView.CoreWebView2.Navigate(_serverHost.Urls.First());
         }
 
-        private static IHost CreateServer()
+        private static WebApplication CreateServer()
         {
             var builder = WebApplication.CreateBuilder();
             builder.WebHost.UseUrls("http://127.0.0.1:0");
@@ -44,15 +41,6 @@ namespace RemindMe
             var app = builder.Build();
 
             app.UseStaticFiles();
-            app.MapGet("/{file}.woff2", async (string file, IWebHostEnvironment env) =>
-            {
-                var b64Path = Path.Combine(env.WebRootPath, $"{file}.woff2.b64");
-                if (!File.Exists(b64Path))
-                    return Results.NotFound();
-                var base64 = await File.ReadAllTextAsync(b64Path);
-                var bytes = Convert.FromBase64String(base64);
-                return Results.File(bytes, "font/woff2");
-            });
             app.MapControllers();
             app.MapFallbackToFile("index.html");
 
